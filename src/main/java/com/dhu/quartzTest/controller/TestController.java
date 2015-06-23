086dcf2bb8b099a76f76ac14e9a23d9242a6f480
easyworld <easy_world_118@hotmail.com>
2015-06-23 16:17:18 +0800

    配合主页改版

--- src/main/java/com/dhu/quartzTest/controller/TestController.java
+++ src/main/java/com/dhu/quartzTest/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.dhu.quartzTest.controller;
 
-import java.util.Map;
+import java.util.List;
+
+import net.sf.json.JSONArray;
 
 import org.quartz.JobDataMap;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dhu.quartzTest.job.TestJob;
+import com.dhu.quartzTest.util.Constant;
 import com.dhu.quartzTest.util.QuartzManager;
 
 @Controller
@@ -26,21 +29,64 @@ public class TestController {
 
 	@RequestMapping("/index")
 	public ModelAndView index() {
-		return new ModelAndView("index");
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("plist", QuartzManager.getPlannedJobList());
+		mav.addObject("rlist", QuartzManager.getRunningJobList());
+		return mav;
 	}
 
-	@RequestMapping(value = "test", produces = "text/html;charset=UTF-8")
-	public @ResponseBody String test(String param) {
+	/**
+	 * 查询计划中任务
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "getPlannedJobList", produces = "text/html;charset=UTF-8")
+	public @ResponseBody String getPlannedJobList() {
+		List<?> list = QuartzManager.getPlannedJobList();
+		String json = JSONArray.fromObject(list).toString();
+		return json;
+	}
+
+	/**
+	 * 查询正在执行的任务
+	 * 
+	 * @param param
+	 * @return
+	 */
+	@RequestMapping(value = "getRunningJobList", produces = "text/html;charset=UTF-8")
+	public @ResponseBody String getRunningJobList(String param) {
+		List<?> list = QuartzManager.getRunningJobList();
+		String json = JSONArray.fromObject(list).toString();
+		return json;
+	}
+
+	@RequestMapping(value = "addJob", produces = "text/html;charset=UTF-8")
+	public @ResponseBody String addJob(String name, String group, String time,
+			String str) {
 		JobDataMap map = new JobDataMap();
-		map.put("str", param);
-		QuartzManager.addJob("test1", TestJob.class, map, "*/5 * * * * ?");
-		QuartzManager.startJobs();
-		return "success";
+		map.put("str", str);
+		try {
+			QuartzManager.addJob(name, group,
+					String.valueOf(System.currentTimeMillis()),
+					Constant.TRIGGER_GROUP_NAME, TestJob.class, map, time);
+			return Constant.SUCCESS;
+		} catch (Exception e) {
+			return e.getMessage();
+		}
 	}
 
-	@RequestMapping(value = "changetime", produces = "text/html;charset=UTF-8")
-	public @ResponseBody String changetime(String param) {
-		QuartzManager.modifyJobTime("test1", param);
-		return "success";
+	@RequestMapping(value = "start", produces = "text/html;charset=UTF-8")
+	public @ResponseBody String start() {
+		try {
+			if (QuartzManager.isStarted()) {
+				QuartzManager.shutdownJobs();
+				return Constant.SHUTDOWN;
+			} else {
+				QuartzManager.startJobs();
+				return Constant.START;
+			}
+		} catch (Exception e) {
+			return e.getMessage();
+		}
 	}
 }
