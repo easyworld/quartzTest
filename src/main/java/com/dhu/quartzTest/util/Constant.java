233065758db1960cb29ce3e97cf5b6901378c123
easyworld <easy_world_118@hotmail.com>
2015-06-23 16:16:50 +0800

    加入常数类

---
+++ src/main/java/com/dhu/quartzTest/util/Constant.java
@@ -0,0 +1,9 @@
+package com.dhu.quartzTest.util;
+
+public class Constant {
+	public static final String JOB_GROUP_NAME = "WEB_JOBGROUP_NAME";
+	public static final String TRIGGER_GROUP_NAME = "WEB_TRIGGERGROUP_NAME";
+	public static final String SUCCESS = "success";
+	public static final String SHUTDOWN = "shutdown";
+	public static final String START = "start";
+}
