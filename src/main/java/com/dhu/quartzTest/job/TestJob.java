58216872874b6e257834549cb4cceb04fe4e8a00
easyworld <easy_world_118@hotmail.com>
2015-06-23 13:09:20 +0800

    继承job接口，重新整理实验功能

--- src/main/java/com/dhu/quartzTest/job/TestJob.java
+++ src/main/java/com/dhu/quartzTest/job/TestJob.java
@@ -2,16 +2,29 @@ package com.dhu.quartzTest.job;
 
 import java.util.Random;
 
-public class TestJob {
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+		System.out.println("任务成功运行");
+		System.out.println("任务名称 = ["
+				+ context.getJobDetail().getKey().getName() + "]");
+		System.out
+				.println("任务输出 = ["
+						+ context.getMergedJobDataMap()
+								.getString("str") + "]");
+	}
+
 	public void workRandom() {
 		Random r = new Random();
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < 100; i++)
 			sb.append((char) (r.nextInt(95) + 32));
 		System.out.println(sb.toString());
-	}
-
-	public void word(String str) {
-		System.out.println("In quartz " + str);
 	}
 }
