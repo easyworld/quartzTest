9b367d752a6443178157ca871f5869c0db726199
royz <royz@Cube6>
2015-10-20 18:37:13 +0800

    加入日志大小

--- src/main/java/com/dhu/quartzTest/util/Constant.java
+++ src/main/java/com/dhu/quartzTest/util/Constant.java
@@ -6,4 +6,5 @@ public class Constant {
 	public static final String SUCCESS = "success";
 	public static final String SHUTDOWN = "shutdown";
 	public static final String START = "start";
+	public static final int NUMBER_OF_LOGS = 100;
 }
